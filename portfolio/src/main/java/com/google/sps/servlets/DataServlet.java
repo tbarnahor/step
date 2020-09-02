0a053afd33379eed7e9331b2acbae8d8ae9a919f
Tal Barnahor <talbarnahor@gmail.com>
2020-09-02 14:44:03 +0000

    remove sort by timestamp because it's causing index errors

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -42,7 +42,8 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     int maxComments = Integer.parseInt(request.getParameter("maxComments"));
-    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+    // TODO: sort by timestamp
+    Query query = new Query("Comment");
     List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(maxComments));
     List<String> comments = new ArrayList<>();
     for (Entity entity : results) {
